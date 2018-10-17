d8ba831b4d2fa5093beb86cbd4d0e8a68caa61c9
bong <chopisal@gmail.com>
2018-10-18 00:18:08 +0900

    route

--- src/Components/Menu/MenuPresenter.tsx
+++ src/Components/Menu/MenuPresenter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import styled from "../../typed-components";
 
 const Container = styled.div`
@@ -13,11 +14,67 @@ const Header = styled.div`
   color: white;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 10px;
+  height: 100%;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 80px;
+  width: 80px;
+  background-color: grey;
+  border-radius: 40px;
+  overflow: hidden;
+`;
+
+const Text = styled.span`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+const Name = styled.h2`
+  font-size: 22px;
+  color: white;
+  margin-bottom: 10px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Rating = styled.h5`
+  font-size: 18px;
+  color: white;
+`;
+
+const SLink = styled(Link)`
+  font-size: 22px;
+  display: block;
+  margin-left: 15px;
+  margin-bottom: 25px;
+  font-weight: 400;
+`;
+
 const MenuPresenter: React.SFC = () => (
   <Container>
     <Header>
-      hello Menu
+      <Grid>
+        <Link to={"/edit-account"}>
+          <Image 
+            src="https://lh3.googleusercontent.com/-CTwXMuZRaWw/AAAAAAAAAAI/AAAAAAAAAUg/8T5nFuIdnHE/photo.jpg" 
+          />
+        </Link>
+        <Text>
+          <Name>Seong Bong</Name>
+          <Rating>4.5</Rating>
+        </Text>
+      </Grid>
     </Header>
+    <SLink to="/trips">Your Trips</SLink>
+    <SLink to="/settings">Settings</SLink>
   </Container>
 );
 
